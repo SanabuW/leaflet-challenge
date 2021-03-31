da0bb493fae04df302ff355a508357a25116ce1d
Sanabu W <sanabuwashizuka@gmail.com>
2021-03-31 12:44:50 -0700

    initial level completed

---
+++ Leaflet-Step-2/static/js/config_local.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoic3dhc2hpIiwiYSI6ImNrbWh6YW80ajBjZG0yb3FteGR4dm40dWoifQ.GIb3ngQ1Ooc4eRYJLd4zLg"
--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -56,7 +56,7 @@ d3.json(queryUrl, function(data) {
     });
 
 
-    // Define streetmap layer
+    // Define streetmap and darkmap layers
     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
       tileSize: 512,
@@ -66,10 +66,17 @@ d3.json(queryUrl, function(data) {
       accessToken: API_KEY
     });
 
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "dark-v10",
+      accessToken: API_KEY
+    });
 
     // Define base layers object
     var baseMaps = {
-      "Street Map": streetmap
+      "Street Map": streetmap,
+      "Dark Map": darkmap
     };
 
     // Define overlay object, holding the earthquake layer
@@ -85,6 +92,11 @@ d3.json(queryUrl, function(data) {
       zoom: 5,
       layers: [streetmap, earthquakes]
     });
+
+    // Create a layer control
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
 
     // Create legend object
     var info = L.control({
@@ -102,7 +114,7 @@ d3.json(queryUrl, function(data) {
     };
       return div;
     };
-
+    
     // Add the legend to the map object
     info.addTo(myMap);
   });
